17f54472855b9621de79896dae6aa0c69615f033
Nikita Bochkarev <405663260nikita@gmail.com>
2023-06-18 21:57:47 +0300

    rename folder

--- todolist_app/core/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    email = models.EmailField("email address", blank=False, null=False)
-    updated = models.DateTimeField(auto_now=True)
