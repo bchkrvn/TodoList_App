ff036eee393d3f7375a30a1ab6e86331f6d20438
Nikita Bochkarev <405663260nikita@gmail.com>
2023-06-25 14:24:11 +0400

    deploy 5

--- api/todolist/settings.py
+++ api/todolist/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'POST': os.getenv('DB_PORT')
     }
