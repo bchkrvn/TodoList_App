55ad9ea90ba1842965e04a3adfdaea019f1da708
Nikita Bochkarev <405663260nikita@gmail.com>
2023-07-03 17:08:52 +0400

    added Category, Goal and Comment factories

--- tests/factories.py
+++ tests/factories.py
@@ -1,7 +1,10 @@
-from factory import Faker
+import datetime
+
+from factory import Faker, SubFactory
 from factory.django import DjangoModelFactory
 
 from core.models import User
+from goals.models import Category, Goal, Comment
 
 
 class UserFactory(DjangoModelFactory):
@@ -12,3 +15,31 @@ class UserFactory(DjangoModelFactory):
     last_name = Faker('last_name')
     email = Faker('email')
     username = Faker('name')
+
+
+class CategoryFactory(DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    user = SubFactory(UserFactory)
+    title = Faker('name')
+
+
+class GoalFactory(DjangoModelFactory):
+    class Meta:
+        model = Goal
+
+    title = Faker('name')
+    description = Faker('sentence', nb_words=10)
+    category = SubFactory(CategoryFactory)
+    user = SubFactory(UserFactory)
+    due_date = datetime.datetime.now()
+
+
+class CommentFactory(DjangoModelFactory):
+    class Meta:
+        model = Comment
+
+    user = SubFactory(UserFactory)
+    goal = SubFactory(GoalFactory)
+    text = Faker('sentence', nb_words=20)
